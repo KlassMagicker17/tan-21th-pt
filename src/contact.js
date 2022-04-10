f1d7411db6feb4fb955413327eb72d923c1b9c34
KlassMagicker17 <75986198+KlassMagicker17@users.noreply.github.com>
2022-04-10 20:41:31 +0800

    footer to bottom of page and contact page upgrade

--- src/contact.js
+++ src/contact.js
@@ -1,25 +1,43 @@
 window.addEventListener("load", function () {
+    const sumbitButton = document.getElementById('submit-button')
     const form = document.getElementById('contact-form');
-    form.addEventListener("submit", function (e) {
+
+    form.addEventListener("submit", async function (e) {
+
         e.preventDefault();
         const data = new FormData(form);
         const action = e.target.action;
+        
+        sumbitButton.classList.add('disabled');
+        sumbitButton.disabled = true;
+
         try {
-            fetch(action, {
-                method: 'POST',
-                body: data
-            }).then(response => {
-                if (response.status === 200) {
-                    alert('success!');
-                } else {
-                    alert("something went wrong...");
-                }
-            })
+            const response = await fetch(action, {method: 'POST', body:data});
+            const responseBody = await response.json();
+
+            if (responseBody.result === 'success') {
+                window.location.href = '/thank-you'
+            } else {
+                showWarning();
+            }
         }
         catch(err) {
-            alert('something went wrong...');
+            showWarning();
             console.log(err);
+        }
+        finally {
+            sumbitButton.classList.remove('disabled')
+            sumbitButton.disabled = false;
+
         }
 
     });
 });
+
+function showWarning() {
+    const sumbitButton = document.getElementById('error-message');
+    sumbitButton.classList.add('animate-stretch-open-class');
+    setTimeout(() => {
+        sumbitButton.classList.remove('animate-stretch-open-class');
+    }, 5000);
+}
